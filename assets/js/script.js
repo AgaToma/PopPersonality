e12b77e0983808d155b50db64d93794e0ef22acb
AgaToma <aga.tomashevska@gmail.com>
2022-12-24 12:49:28 +0000

    Add name box open function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 let rulesModal = document.getElementById("rules-modal");
 let rulesOpenButton = document.getElementById("open-modal");
 let rulesClose = document.getElementsByClassName("close-modal")[0];
+let quizStartButton = document.getElementById("start-btn");
+let nameBox = document.getElementById("name-box");
 
 rulesOpenButton.addEventListener("click", openRulesModal);
 rulesClose.addEventListener("click", closeRulesModal);
+quizStartButton.addEventListener("click", openNameBox);
+
 
 //Opens rules modal when user clicks Rules button
 function openRulesModal () {
@@ -13,4 +17,9 @@ function openRulesModal () {
 //Closes rules modal when user clicks x 
 function closeRulesModal () {
     rulesModal.style.display = "none"
+}
+
+//Opens name input form
+function openNameBox () {
+    nameBox.style.display = "block";
 }
