c7ea8af498e6670c55935f8a170fd5689ddca37d
AgaToma <aga.tomashevska@gmail.com>
2022-12-24 12:35:00 +0000

    Add rules modal

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+let rulesModal = document.getElementById("rules-modal");
+let rulesOpenButton = document.getElementById("open-modal");
+let rulesClose = document.getElementsByClassName("close-modal")[0];
+
+rulesOpenButton.addEventListener("click", openRulesModal);
+rulesClose.addEventListener("click", closeRulesModal);
+
+//Opens rules modal when user clicks Rules button
+function openRulesModal () {
+    rulesModal.style.display = "block";
+}
+
+//Closes rules modal when user clicks x 
+function closeRulesModal () {
+    rulesModal.style.display = "none"
+}
