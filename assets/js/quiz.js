ca1f4c789c0962a27137e300208e91d997d2b596
AgaToma <aga.tomashevska@gmail.com>
2022-12-25 13:05:00 +0000

    Add questions array and quiz area variables

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,13 +1,16 @@
+//quiz intro area variables
 let nameBox = document.getElementById("name-box");
 let introNextButton = document.getElementById("intro-next");
 let userName = document.getElementById("name");
 let ageBox = document.getElementById("age-box");
 let userAge = document.getElementById("age-input");
 let startQuizButton = document.getElementById("start-quiz");
-let question = document.getElementById("question");
 
+//quiz intro event listeners
 introNextButton.addEventListener("click", showAgeBox);
 startQuizButton.addEventListener("click", validateAgeInput);
+
+//quiz intro functions
 
 /** 
  * Submits name input, hides input and shows age box
@@ -45,6 +48,29 @@ function validateAgeInput (event) {
 
 }
 
+//quiz area variables
+
+let question = document.getElementById("question");
+//answers array - to be added
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+//question for questions to be selected from minus questions already used
+let availableQuestions = [];
+
+//all questions
+let questions = [
+    {question: "Is your room clean and in order?"},
+    {question: "Is your homework always done on time?"},
+    {question: "Do you like cooking?"},
+    {question: "Do you often help others when they are stuck?"},
+    {question: "Do you like when it snows?"},
+];
+
+
 //distributes user to correct age group and starts quiz
+
 
 
